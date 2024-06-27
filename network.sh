5b0b32d38efda1908665bbf9af45f2a6606eb291
anisomorphic <42658726+anisomorphic@users.noreply.github.com>
2024-06-27 03:44:25 -0400

    some tweaks and additions based on network changes

--- network.sh
+++ network.sh
@@ -1,7 +1,20 @@
-WAN_EXT_IP="98.137.246.7"
+#Network utility that verifies connectivity - should be run in a loop every few seconds 
+#		sleep 2 && while true; do /Users/you/network.sh -critonly ; sleep 2; done
+#		sleep 2 && while true; do /Users/you/network.sh ; sleep 2; done
+
+
+
+#############################################
+################# Constants #################
+#############################################
+
+CRIT_ONLY_FLAG="-critonly"
+
+WAN_EXT_IP="142.250.72.78"
 WAN_EXT_HOST="google.com"
-UP_TEXT="UP"
-DOWN_TEXT="DOWN"
+ROUTER_INT_IP="10.0.0.1"
+#MODEM_INT_IP="192.168.100.1" #in case your router/modem has a second login interface at a different IP
+
 FORMAT1="\e[4;32m"
 FORMAT1a="\e[32m"
 FORMAT2="\e[4;31m"
@@ -9,29 +22,55 @@ FORMAT2a="\e[31m"
 FORMAT3="\e[1;34m"
 FORMAT4="\e[90m"
 CLRFMT="\e[0m"
-HEADER_TEXT1="  =="
-HEADER_TEXT2="==  "
-ROUTER_INT_IP="192.168.0.30"
-MODEM_INT_IP="192.168.100.1"
-FUNCTIONAL_TEXT="All equipment operational"
-PROBLEMS_TEXT="Some equipment non-functional"
+
 CRIT_IT="Critical infrastructure "
+UP_TEXT="UP"
+DOWN_TEXT="DOWN"
+
+FUNCTIONAL_TEXT="All equipment operational"
+PROBLEMS_TEXT="Some equipment inactive"
+
+HEADER_TEXT1="  ========"
+HEADER_TEXT2="========  "
+HEADER_TEXT3=" └"
+HEADER_TEXT4=": "
+
 FUNCTIONAL_CRIT_TEXT="$CRIT_IT$FORMAT1$UP_TEXT$CLRFMT$FORMAT1a"
 PROBLEMS_CRIT_TEXT="$CRIT_IT$FORMAT2$DOWN_TEXT$CLRFMT$FORMAT2a"
 
+
+
+
+
+
+#############################################
+################# Variables #################
+#############################################
 
 FAILURE="0"
 CRIT_FAILURE="0"
 
 
 
+
+
+
+#############################################
+################# Functions #################
+#############################################
+
 function _test {
-if ping -q -c 1 -W 1 $1 >/dev/null; then
-  printf '%b' "$FORMAT1a$2$CLRFMT  "
-else
-  printf '%b' "$FORMAT2a$2$CLRFMT  "
-  FAILURE="1"
-fi
+  if [ "$3" = $CRIT_ONLY_FLAG ]; then
+    : #dont want to process this non critical test if we are in crit only mode
+  else
+    if ping -q -c 1 -W 1 $1 >/dev/null; then
+      printf '%b' "$FORMAT1a$2$CLRFMT  "
+    else
+      printf '%b' "$FORMAT2a$2$CLRFMT  "
+      FAILURE="1"
+    fi
+  fi
+
 }
 
 function _test_crit {
@@ -51,7 +90,7 @@ else
   _intro
 fi
 
-printf '%b\n' "$FORMAT4$HEADER_TEXT1$FORMAT3$1$CLRFMT$FORMAT4$HEADER_TEXT2"
+printf '%b\n' "$FORMAT4$HEADER_TEXT3$FORMAT3$1$CLRFMT$FORMAT4$HEADER_TEXT4"
 }
 
 function _header2 {
@@ -61,7 +100,7 @@ else
   _intro
 fi
 
-printf '%b' "$2$FORMAT4$FORMAT3$1$CLRFMT$FORMAT4 - "
+printf '%b' "$2$FORMAT4$FORMAT3$1$CLRFMT$FORMAT4: "
 }
 
 
@@ -82,62 +121,163 @@ echo $result
 
 function _intro {
 clear
-#printf ' --- Network check utility v2.0 --- \n'
+#printf ' --- Network check utility v3.0 --- \n'
 printf 'Current IP: '
 _ip
-echo && echo
+echo
 }
 
-_header2 Internet "      " 0
+
+
+
+
+
+
+
+#############################################
+################# Handling #################
+#############################################
+
+
+
+if [ "$1" = $CRIT_ONLY_FLAG ]; then
+  clear &&
+  _header2 Internet "      "
+else
+  _header2 Internet "      " 0
+fi
+
+
+
 _test_crit $WAN_EXT_IP WAN
 _test_crit $WAN_EXT_HOST DNS
 
-_header2 Infrastructure
-_test_crit $MODEM_INT_IP Modem
-_test_crit $ROUTER_INT_IP Router
-_test_crit 192.168.0.31 pi
-_test 192.168.0.200 NAS
+_header2 Infrastructure 
+#_test_crit $MODEM_INT_IP Modem
+_test_crit $ROUTER_INT_IP Modem #Gateway
+#_test_crit 192.168.0.29 Wi-Fi
+_test 10.0.0.200 NAS $1
+_test_crit 10.0.0.31 pi3B+
+_test 10.0.0.32 pivm $1
 
 _header2 Switches "      "
-_test 192.168.0.10 *.10 #- not connected
-_test_crit 192.168.0.11 *.11
-_test_crit 192.168.0.12 *.12
+#_test 192.168.0.10 *.10
+#_test_crit 192.168.0.11 *.11
+#_test 192.168.0.12 *.12
+#_test_crit 192.168.0.14 *.14
+#_test_crit 192.168.0.15 *.15
+#_test 192.168.0.16 *.16 $1
+_test_crit 10.0.0.20 "Core_10Gb"
+_test_crit 10.0.0.11 "\n\t\tCore_Ethernet"
+_test 10.0.0.14 "\n\t\tEntertainment" $1
+_test 10.0.0.15 "Downstairs_Office" $1
+printf "\n\t\t"
+_test 10.0.0.16 "Upstairs_Office#1" $1
+_test 10.0.0.17 "Upstairs_Office#2" $1
 
 echo
 
-if [ "$CRIT_FAILURE" -eq 0 ]; then 
-  printf '%b' "\n $FORMAT4$HEADER_TEXT1$FORMAT1a$HEADER_TEXT2$FUNCTIONAL_CRIT_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT"; 
-else 
-  printf '%b' "\n $FORMAT4$HEADER_TEXT1$FORMAT2a$HEADER_TEXT2$PROBLEMS_CRIT_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT"; 
+if [ "$CRIT_FAILURE" -eq 0 ]; then
+  printf '%b' "\n $FORMAT4$HEADER_TEXT1$FORMAT1a$HEADER_TEXT2$FUNCTIONAL_CRIT_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT";
+else
+  printf '%b' "\n $FORMAT4$HEADER_TEXT1$FORMAT2a$HEADER_TEXT2$PROBLEMS_CRIT_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT";
 fi
 
+
+if [ "$1" = $CRIT_ONLY_FLAG ]; then
+  echo && echo && exit
+fi
+
+
+
+
+
+
+_header Servers
+_device "NUC" "         "
+_test nuc.local "eth0"
+echo
+_device "Mac Pro" "     "
+_test 10.0.0.201 "eth0"
+#_test 192.168.0.202 "eth1"
+#_device "Xserve" "   "
+#_test 192.168.0.211 "eth0"
+#_test 192.168.0.212 "eth1"
+echo
+_device "mini-serv" "   "
+_test 10.0.0.210 "eth0"
+#echo
+#_device "netbook" "     "
+#_test 192.168.0.131 "eth0"
+
+
+
 _header Computers
-_device "Mac Pro" "  "
-_test 192.168.0.201 "eth0"
-_test 192.168.0.202 "eth1"
+_device "m1ni" "        "
+_test 10.0.0.202 "eth0"
 echo
-_device "Xserve" "   "
-_test 192.168.0.211 "eth0"
-_test 192.168.0.212 "eth1"
+_device "Aurora" "      "
+_test aurora.local "eth0"
+#echo
+#_device "netbook" "     "
+#_test 192.168.0.131 "eth0"
+
+
+
+_header Laptops
+#_device "Razer 14\"" "   "
+#_test 10.0.0.202 "Connected"
+#echo
+_device "MacBook Pro" " "
+_test macbook-pro.local "Connected"
+
+
+
+_header AppleTV
+_device "Bedroom" "     "
+_test bedroom-tv.local eth0
 echo
-_device "GamingPC" " "
-_test 192.168.0.203 "eth0"
-_test 192.168.0.204 "eth1"
+_device "Downstairs" "  "
+_test living-room-tv.local eth0
+
 
 
 _header "Airport Nodes"
-_test 192.168.0.35 Bedroom
-_test 192.168.0.36 Bookcase
-_test 192.168.0.37 Desk
+_device "Bedroom" "     "
+_test 10.0.0.35 eth0
+echo
+_device "Office" "      "
+_test 10.0.0.36 eth0
+echo
+_device "Living Room" " "
+_test 10.0.0.37 eth0
+
+
 
 _header Wireless
-_test 192.168.0.99 iPhone6S+
-_test 192.168.0.111 Netbook
+_device "iPhone" "      "
+_test tiphone.local "15 Pro Max"
+#_test knowledavigator.local "12 Pro Max"
+#_test iphone6s.local 6S+
+
+
+
+#_header Wireless
+#_test 192.168.0.99 iPhone6S+
+#_test 192.168.0.111 Netbook
+
+
+
+#_header Wireless
+#_test 192.168.0.99 iPhone6S+
+#_test 192.168.0.111 Netbook
 
 echo
 
-if [ "$FAILURE" -eq 0 ]; then 
-  printf '%b\n\n' "\n $FORMAT4$HEADER_TEXT1$FORMAT1a$HEADER_TEXT2$FUNCTIONAL_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT"; 
-else 
-  printf '%b\n\n' "\n $FORMAT4$HEADER_TEXT1$FORMAT2a$HEADER_TEXT2$PROBLEMS_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT"; 
+if [ "$FAILURE" -eq 0 ]; then
+  printf '%b\n\n' "\n $FORMAT4$HEADER_TEXT1$FORMAT1a$HEADER_TEXT2$FUNCTIONAL_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT";
+else
+  printf '%b\n\n' "\n $FORMAT4$HEADER_TEXT1$FORMAT2a$HEADER_TEXT2$PROBLEMS_TEXT$HEADER_TEXT1$FORMAT4$HEADER_TEXT2$CLRFMT";
 fi
+
+
